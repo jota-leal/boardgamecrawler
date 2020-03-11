6fc69e0b01789cc7f71e52963a26d71b48633314
Juan Leal <jota-leal@users.noreply.github.com>
2020-03-11 16:23:42 -0300

    Script principal y clase de colores

---
+++ bgc.py
@@ -0,0 +1,24 @@
+import sys
+from stores.elogroalegre import elogroalegre
+from stores.invictvs import invictvs
+from stores.lacuevadelokami import lacuevadelokami
+from stores.losjuegosdeulthar import losjuegosdeulthar
+from stores.ludisterra import ludisterra
+from stores.mrdm import mrdm
+from bcolors import bcolors
+
+items = []
+items.extend(elogroalegre.getItems(sys.argv[1]))
+items.extend(invictvs.getItems(sys.argv[1]))
+items.extend(lacuevadelokami.getItems(sys.argv[1]))
+items.extend(losjuegosdeulthar.getItems(sys.argv[1]))
+items.extend(ludisterra.getItems(sys.argv[1]))
+items.extend(mrdm.getItems(sys.argv[1]))
+
+print
+for item in items:
+    stock = ''
+    if not item['stock']:
+        stock = bcolors.FAIL + 'Sin stock' + bcolors.ENDC + ' | '
+    print(bcolors.OKBLUE + item['store'] + bcolors.ENDC + ' | ' + bcolors.HEADER + item['title'].encode('utf-8') +
+          bcolors.ENDC + ' | ' + bcolors.OKGREEN + item['price'] + bcolors.ENDC + ' | ' + stock + item['url'])
